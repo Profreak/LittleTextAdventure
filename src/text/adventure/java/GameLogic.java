3ac00610853394cfe662c6404e0b2f4817911871
MrAATiger <alex.angeloussis@web.de>
2015-01-07 22:01:27 +0100

    neu

--- src/text/adventure/java/GameLogic.java
+++ src/text/adventure/java/GameLogic.java
@@ -5,135 +5,58 @@ import java.util.Random;
 
 public class GameLogic {
 	
+	Player player = new Player ();
+	Enemy enemy = new Enemy ();
+	boolean running = true;
+	Scanner sc = new Scanner (System.in);
+	String  action = sc.next();
 	
-
-	public void GameLogic() {
-
-		Enemy enemy = new Enemy();
-		enemy.getEnemy();
-		 
-		Player player = new Player();
-		player.player();
-		int enemyHealth = enemy.enemyHealth;
+	
+	public void begrüßung () {
 		
-		Random rand = new Random ();
+		System.out.println ("Welcome to the Dungeon!");
+		
+	}
+	
+	public void gameSchleife () {
+		
+		while (running) {
+			
+			System.out.println ("What do you want to do?");
+			System.out.println ("\t1. Go deeper to the Dungeon!");
+			System.out.println ("\t2. Take a Healpot");
+			System.out.println ("\t3. Search the area");
+			System.out.println ("\t4. leave the Dungeon");
+		
+		}
+		
+	}
+	
+	
+	
+	public void wennBefehle () {
+		if (action.equals("1")) {
+			System.out.println ("You go deeper into the Dungeon");
+		}
+		
+	}
+	
+	
+	public void pause() {
 
+		
 		System.out.println("Welcome to the Dungeon!");
 
 		// pausiert das Spiel um 1 Sekunde! Ist in double ... Es sind also auch
 		// 0.5 sekunden moeglich
 		// bsp: pause(0.5);
 		pause(1);
-
-		
-
-			System.out.println("#-----------------------------#");
-			
-			System.out.println ("What do you you want to do?");
-
-			System.out.println("\t1. Go further down into the Dungeon!");
-			System.out.println("\t2. Search this area");
-			System.out.println("\t3. Leave the Dungeon");
-			System.out.println("\t4. Heal");
-
-			Scanner sc = new Scanner(System.in);
-			boolean running = true;
-			while (running) {
-				
-				System.out.print("Eingabe: ");
-				String action = sc.next();
-				
-			
-			if (action.equals("1")) {
-				System.out.println("An" + "" + enemy.enemy + "appeared");
-
-				
-				
-				System.out.println(" What will you do?");
-				System.out.println("\t1. Attack!");
-				System.out.println("\t2. Defence");
-				System.out.println("\t3. Run!");
-				System.out.println("\t4. Heal");
-				
-				String action1 = sc.next ();
-				
-				Game:
-				while (enemy.isAlive()) {
-					if (action1.equals("1")) {
-						
-						
-						
-						int damageDone = rand.nextInt (player.playerAttack);
-						enemy.setEnemyHealth(enemy.getEnemyHealth() - damageDone);
-						
-						int damageTaken =enemy.getEnemyAttack();
-						player.playerHealth -= damageTaken;
-						
-						System.out.println ("You have " + player.playerHealth +" HP");
-						System.out.println ("You deal " + damageDone + " damage");
-						System.out.println ("You take " + damageTaken + " damage");
-						System.out.println ("You have " + player.playerHealth + " left");
-						System.out.println ("The enemy has " + enemyHealth + " left");
-						
-						if (enemyHealth < 1) {
-							System.out.println ("You defeated the enemy");
-						}
-				
-						else if (enemyHealth> 1) {
-							continue Game;
-						}
-				
-					}
-					
-					if (action1.equals("2")) {
-						
-						int defence =rand.nextInt(player.playerDefence);
-						int damageTaken =enemy.getEnemyAttack() - defence; 
-						player.playerHealth -= damageTaken;
-						if (damageTaken<1){
-							damageTaken = 0;
-						}
-						System.out.println ("You manage to rise you shield");
-						System.out.println ("You take " + damageTaken + " damage");
-						
-						if (player.playerHealth > 1) {
-							System.out.println ("The fight goes on");
-							System.out.println ("You take " + damageTaken + " damage");
-							System.out.println ("You have " + player.playerHealth +" HP");
-							System.out.println ("You have " + player.playerHealth + " HP left");
-							System.out.println ("The enemy has " + enemyHealth + " HP left");
-							
-						}
-					}
-				
-					
-				}
-				
-					
-			} else if (action.equals("2")) {
-				System.out.println("You are searching this area for something");
-
-			} else if (action.equals("3")) {
-				System.out.println("You are leaving the Dungeon");
-				System.out.println("#----------------------------#");
-				System.out.println("Thank you for playing!");
-				break;
-
-			} else if (action.equals("4")) {
-				player.playerHealPots--;
-				player.playerHealth += player.healPots;
-				System.out.println("You drink a heal pot");
-				System.out.println ("You have now " + player.playerHealth + " HP");
-				System.out.println ("You have still " + player.playerHealPots + " Healpots left");
-			}
-
-			else {
-				System.out.println("invalid command");
-			}
-
 		}
-
+	public void attack () {
+		
+		
 	}
+	
 
 	/**
 	 * paused the game for n seconds
