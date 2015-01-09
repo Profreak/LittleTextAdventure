ba205ccb77d89d6102454fdc28b244a7bb194a6b
MrAATiger <alex.angeloussis@web.de>
2015-01-09 17:14:20 +0100

    Logic

--- src/text/adventure/java/Enemy.java
+++ src/text/adventure/java/Enemy.java
@@ -11,45 +11,47 @@ import java.util.Random;
  */
 public class Enemy {
 
-
-
-	public final static int  MAX_ENEMY_HEALTH = 50;
+	public final static int MAX_ENEMY_HEALTH = 50;
 	public final static int MAX_ENEMY_ATTACK = 35;
 	public final static int MAX_ENEMY_DEFENCE = 5;
 	public final static int MIN_ENEMY_DEFENCE = 1;
-	public final static int MIN_ENEMY_ATTACK =1;
+	public final static int MIN_ENEMY_ATTACK = 1;
 	
-	public String [] enemies = {"Zombie","Knight", "Mage"};
+
+	public String[] enemies = { "Zombie", "Knight", "Mage" };
+	private Random rand = new Random();
+	public String enemy; 
 	
-	Random rand = new Random ();
-	
-	
-	
-	
-	public Enemy () {
-	
-		String enemy = enemies [rand.nextInt(enemies.length)];
-		
-		}
-	
-	public void enemyAttack () {
+	public Enemy() {
+
+		enemy = enemies[rand.nextInt(enemies.length)];
+
+	}
+
+	public int enemyAttack() {
 		int attack = rand.nextInt(MAX_ENEMY_ATTACK);
 		if (attack < 1) {
 			attack = 1;
+
 		}
+		return attack;
 	}
-	
-	public void enemyHealth () {
+
+	public int enemyHealth() {
 		int health = rand.nextInt(MAX_ENEMY_HEALTH);
 		if (health < 10) {
 			health = 10;
 		}
+		return health;
+	
 	}
 	
-	public void enemyDefence () {
+
+	public int enemyDefence() {
 		int defence = rand.nextInt(MAX_ENEMY_DEFENCE);
 		if (defence < 1) {
 			defence = 0;
 		}
+		return defence;
 	}
 }
