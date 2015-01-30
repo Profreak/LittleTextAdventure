f94c01c8c95aeb3df193e47e2e94d4aea3ccae2e
MrAATiger <alex.angeloussis@web.de>
2015-01-30 18:41:44 +0100

    taschenrechner

---
+++ src/neues/java/programm/Ausführung.java
@@ -0,0 +1,45 @@
+package neues.java.programm;
+
+import java.util.Scanner;
+
+
+public class Ausführung {
+
+Scanner sc;
+String eingabe;
+String eingabe1;
+	
+
+
+public Ausführung (){
+
+sc = new Scanner (System.in);
+}
+
+public void begrüßung  () {
+	System.out.println ("Hallo lieber Nutzer");
+	System.out.println ("Bitte tätigen Sie eine Eingabe");
+	System.out.println ("==>");
+	this.eingabe1();
+	
+}
+
+public void eingabe1 () {
+	this.eingabe = sc.next ();
+	int zahl1 = Integer.parseInt((eingabe));
+	
+}
+
+public void eingabe () {
+	
+}
+
+public void eingabe2 () {
+	this.eingabe1 = sc.next();
+	int zahl2 = Integer.parseInt(eingabe1);
+	
+	
+}
+
+
+}
---
+++ src/neues/java/programm/Taschenrechner.java
@@ -0,0 +1,18 @@
+package neues.java.programm;
+
+
+
+
+public class Taschenrechner {
+
+
+	
+
+	public static void main(String[] args) {
+		
+		Ausführung blubb = new Ausführung ();
+		blubb.begrüßung ();
+	
+	}
+	
+}
